2622a75b8e82e7bba64f8e1beb4871da79cb9d99
jose <josefrias99@ua.pt>
2021-05-28 01:24:34 +0100

    changes in main app

---
+++ household/src/test/java/pt/ua/household/HouseHoldApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.ua.household;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseHoldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
