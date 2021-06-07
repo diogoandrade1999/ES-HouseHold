668a4d288197e7486320640a4b173db0a3bdc183
diogoandrade1999 <diogo.andrade@ua.pt>
2021-06-07 07:26:02 +0100

    jenkins

--- temperature/src/test/java/ua/pt/temperature/TemperatureApplicationTests.java
+++ temperature/src/test/java/ua/pt/temperature/TemperatureApplicationTests.java
@@ -2,8 +2,10 @@ package ua.pt.temperature;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:test.properties")
 class TemperatureApplicationTests {
 
 	@Test
