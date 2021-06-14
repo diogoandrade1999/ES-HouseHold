005c96a63d9a141d73fa411b17943c065974835d
diogoandrade1999 <diogo.andrade@ua.pt>
2021-06-14 08:46:55 +0100

    elk

--- temperature/src/main/java/ua/pt/temperature/controllers/TemperatureController.java
+++ temperature/src/main/java/ua/pt/temperature/controllers/TemperatureController.java
@@ -25,12 +25,14 @@ public class TemperatureController {
 
     @RequestMapping(value = "/{startDate}/{endDate}", method = RequestMethod.GET)
     public Iterable<Temperature> betweenDates(@PathVariable long startDate, @PathVariable long endDate) {
+        this.logger.info("GET -> Temperature by Date");
         return this.temperatureService.getTemperatureByDate(new Date(startDate * 1000L), new Date(endDate * 1000L));
     }
 
     @RequestMapping(value = "/{startDate}/{endDate}/{houseId}", method = RequestMethod.GET)
     public Iterable<Temperature> betweenDatesAndHouse(@PathVariable long startDate, @PathVariable long endDate,
             @PathVariable long houseId) {
+        this.logger.info("GET -> Temperature from House ID: {}", houseId);
         return this.temperatureService.getTemperatureByDateAndHouse(new Date(startDate * 1000L),
                 new Date(endDate * 1000L), houseId);
     }
@@ -38,12 +40,14 @@ public class TemperatureController {
     @RequestMapping(value = "/{startDate}/{endDate}/{houseId}/{roomId}", method = RequestMethod.GET)
     public Iterable<Temperature> betweenDatesAndHouseAndRoom(@PathVariable long startDate, @PathVariable long endDate,
             @PathVariable long houseId, @PathVariable long roomId) {
+        this.logger.info("GET -> Temperature from Room ID: {}", roomId);
         return this.temperatureService.getTemperatureByDateAndHouseAndRoom(new Date(startDate * 1000L),
                 new Date(endDate * 1000L), houseId, roomId);
     }
 
     @RequestMapping(method = RequestMethod.POST)
     public Temperature save(@RequestBody Temperature temperature) {
+        this.logger.info("POST -> Save Temperature from Room ID: {}", temperature.getRoomId());
         return this.temperatureService.saveTemperature(temperature);
     }
 
